27ac74cee9a4d02fcff7410072760212321ab5cf
kuglitsd <daniel.kuglitsch@infineon.com>
2022-12-05 08:33:20 +0100

    Add project files.

---
+++ FileHandle_blocks_cut_paste/Program.cs
@@ -0,0 +1,30 @@
+﻿//Setup
+var dir = Directory.GetCurrentDirectory();
+//File used in handle
+var handleFilePath = Path.Combine(dir, "handleFile.txt");
+using (File.Create(handleFilePath)) { }
+
+//File used to cut/copy paste
+var moveFilePath = Path.Combine(dir, "moveFile.txt");
+using (File.Create(moveFilePath)) { }
+
+//Blocks File.Move
+File.OpenHandle(
+    handleFilePath,
+    mode: FileMode.Open,
+    access: FileAccess.Read,
+    share: FileShare.Delete | FileShare.ReadWrite
+);
+
+//Will throw Exception
+try
+{
+    File.Move(moveFilePath, handleFilePath, true);
+}
+catch (Exception ex)
+{
+    System.Diagnostics.Debugger.Break();
+}
+
+//Works
+File.Copy(moveFilePath, handleFilePath, true);
